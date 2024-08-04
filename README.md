67cb6a8fe71cbc912c46872bea3c46def5d7b1f1
Márió Hegyi <mario.hegyi93@gmail.com>
2024-08-04 15:56:54 +0200

    Version 0.1
    initial working application

---
+++ README.md
@@ -0,0 +1,20 @@
+# 1 billion row challenge in rust #
+
+Current version: 0.1
+
+## Build and run ## 
+
+- clone the repository
+- run the src/resource/create_measurements.py python file with 1_000_000_000 to generate the test data in the data folder
+- build and run with release flag: `cargo run --release`
+- 1 extra parameter can be provided, which is the file name in the data folder if that is not measurements.txt eg: `cargo run --release another_measurement.txt`
+
+> Disclaimer: these result where achived on a 2018 Mac mini with Intel i5 and 8 Gb of RAM
+
+## Version history ## 
+
+### Version 0.1 ### 
+
+**Running time:** 630 seconds
+
+Very basic implementation just to have a working solution
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,205 @@
-use std::{fs, io::{BufReader, Read}};
+use core::panic;
+use std::path::{Path, PathBuf};
+use std::env;
+use std::fs;
 
-const PATH: &str = "/Users/mariohegyi/Work/rust/one-brc/data/measurements.txt";
+static MAX_VAL: f64 = 100.0;
+static MIN_VAL: f64 = -100.0;
 
-//type MyMap = std::collections::HashMap<String, Vec<String>>;
+type MyMap = std::collections::HashMap<String, Vec<f64>>;
+// key, min, mean, max 
+type MyResult = std::collections::HashMap<String, (String, String, String)>;
+type MyOrderedResult = std::collections::BTreeMap<String, (String, String, String)>;
 
-fn with_buff(path: &str) {
-    //let mut map: MyMap = std::collections::HashMap::new();
-    let mut buffer = [0; 10240];
+fn parse_line(my_map: &mut MyMap, line: &str) -> Option<bool> {
+    let values: Vec<&str> = line.split(";").collect(); 
 
-    let input = fs::File::open(path).unwrap();
-    let mut reader = BufReader::new(input);
-    
-    let count = reader.read(&mut buffer).unwrap();
+    let name = String::from(*values.get(0)?);
+    let value = (*values.get(1)?).parse().expect("Invalid value found!");
 
-    println!("Count {count}");
-    println!("{:?}", buffer);
-    println!("{}", reader.capacity());
+    if my_map.contains_key(&name) {
+        my_map.get_mut(&name)?.push(value);
+    } else {
+        my_map.insert(name, vec![value]);
+    }
+    Some(true)
+}
+
+fn round_value(value: f64) -> String {
+    format!("{:.1}", (value * 10.0).round() / 10.0)
+}
+
+fn calculate_values(input: &Vec<f64>) -> (String, String, String) {
+    let mut max = MIN_VAL;
+    let mut min = MAX_VAL;
+    let mut sum = 0.0;
+
+    for value in input.iter() {
+        if value > &max {
+            max = *value;
+        }
+
+        if value < &min {
+            min = *value;
+        }
+
+        sum += *value;
+    }
+
+    let mean = sum / input.len() as f64;
+
+    (round_value(min), round_value(mean), round_value(max))
+}
+
+fn calculate_result(my_map: &MyMap) -> MyResult {
+    let mut result: MyResult = std::collections::HashMap::with_capacity(my_map.len());
+
+    for (key, value) in my_map.iter() {
+       let number_results = calculate_values(value); 
+
+       result.insert(key.clone(), number_results);
+    }
+
+    result
+}
+
+fn order_result(result: MyResult) -> MyOrderedResult {
+    let mut map = std::collections::BTreeMap::new();
+
+    for (key, values) in result {
+        map.insert(key.clone(), values.clone());
+    }
+
+    map
+}
+
+fn display_result(result: &MyOrderedResult, max_line: usize) {
+    for (city, (min, mean, max)) in result.iter().take(max_line) {
+        println!("{city};{min};{mean};{max}");
+    } 
+}
+
+fn run(path: &Path) -> (usize, MyOrderedResult) {
+    let mut line_counter = 0;
+    let mut my_map: MyMap = std::collections::HashMap::new();
+
+    for line in fs::read_to_string(path).unwrap().lines() {
+        line_counter += 1;
+        parse_line(&mut my_map, line);
+
+        if line_counter % 10_000_000 == 0 {
+            println!("{line_counter}");
+        }
+    }
+
+    let result = order_result(calculate_result(&my_map));
+
+    display_result(&result, result.len());
+
+    (line_counter, result)
+}
+
+fn get_file_name() -> PathBuf {
+    let path = Path::new("data/");
+
+    let path = match env::args().len() {
+        2 => {
+            let file_name = env::args().last().unwrap();
+
+            path.join(file_name)
+        }
+        _ => path.join("measurements.txt"),
+    };
+
+    if let Err(_) = path.try_exists() {
+        panic!("Invalid file path: {:?}", path.to_str());
+    }
+
+    path 
 }
 
 fn main() {
-    //let mut map: MyMap = std::collections::HashMap::new();
+    let path = get_file_name();
+    let now = std::time::Instant::now();
 
-    with_buff(PATH);
+    let (line_counter, result) = run(&path);
 
-    //for line in fs::read_to_string(PATH).unwrap().lines() {
-    //    let raw_split: Vec<&str> = line.split(";").collect();
-    //
-    //    let name = raw_split.get(0).unwrap().to_string();
-    //    let data = raw_split.get(1).unwrap().to_string();
-    //
-    //    if map.contains_key(&name) {
-    //        if let Some(entry_value) = map.get_mut(&name) {
-    //            entry_value.push(data);
-    //        }
-    //    } else {
-    //        map.insert(name, vec![data]);
-    //    }
-    //}
+    let elapsed_time = now.elapsed();
 
-    //println!("Content length: {:?}", map);
+    println!("----STATS----");
+    println!("lines read: {}, cities: {}, elapsed time: {}", line_counter, result.len(), elapsed_time.as_secs());
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_line_parser() {
+        let test_str = String::from("Ranst;-13.7");
+        let test_str2 = String::from("Ranst;2.7");
+        let test_str3 = String::from("Longhua;-5.3");
+
+        let mut my_map: MyMap = std::collections::HashMap::new();
+
+        parse_line(&mut my_map, &test_str);
+
+        assert_eq!(my_map.get("Ranst").unwrap(), &vec![-13.7]);
+
+        parse_line(&mut my_map, &test_str2);
+
+        assert_eq!(my_map.len(), 1);
+        assert_eq!(my_map.get("Ranst").unwrap(), &vec![-13.7, 2.7]);
+
+        parse_line(&mut my_map, &test_str3);
+
+        assert_eq!(my_map.len(), 2);
+        assert_eq!(my_map.get("Longhua").unwrap(), &vec![-5.3]);
+        assert_eq!(my_map.get("Ranst").unwrap(), &vec![-13.7, 2.7]);
+    }
+
+    #[test]
+    fn test_round_value() {
+        let input = 10.0;
+        
+        assert_eq!(round_value(input), String::from("10.0"));
+
+        let input = -20.0;
+
+        assert_eq!(round_value(input), String::from("-20.0"));
+
+        let input = 1.0 / 3.0 * 2.0;
+
+        assert_eq!(round_value(input), String::from("0.7"));
+    }
+
+    #[test]
+    fn test_calculate_values() {
+        let input = vec![10.0, -20.0, 5.0];
+
+        assert_eq!(calculate_values(&input), (
+                String::from("-20.0"), 
+                String::from("-1.7"),
+                String::from("10.0")))
+    }
+
+    #[test]
+    fn test_order_cities() {
+        let mut input: MyResult = std::collections::HashMap::new();
+
+        let tmp = "".to_string();
+
+        let tmp_data = (tmp.clone(), tmp.clone(), tmp.clone());
+
+        input.insert("ACity".to_string(), tmp_data.clone());
+        input.insert("CCity".to_string(), tmp_data.clone());
+        input.insert("BCity".to_string(), tmp_data.clone());
+
+
+        let res = order_result(input);
+        
+        let keys: Vec<_> = res.keys().cloned().collect();
+
+        assert_eq!(keys, vec!["ACity", "BCity", "CCity"]);
+    }
+}
